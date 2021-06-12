eb83fc3ca190c3cc4263ebfc7fc1f48bf7bc362f
Aleksander <57395146+AleksanderEZ@users.noreply.github.com>
2021-06-12 21:38:55 +0100

    Added small interaction and functioning example

--- src/engine/Window.java
+++ src/engine/Window.java
@@ -1,10 +1,10 @@
 package engine;
 
-import fakemon.TestResolution;
 import org.lwjgl.Version;
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.opengl.GL;
 
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.system.MemoryUtil.NULL;
@@ -16,14 +16,21 @@ public class Window {
     String title;
     private long glfwWindow;
 
+    private float a, r, g, b;
+
 //    Singleton
     private static Window window = null;
+    private boolean fadeToBlack;
 
     private Window() {
         getScreenInfo = TestResolution.display();
         this.width = getScreenInfo[0];
         this.height = getScreenInfo[1];
         this.title = "Fakemon";
+        a = 1;
+        r = 1;
+        g = 1;
+        b = 1;
     }
 
     public static Window get(){
@@ -38,6 +45,12 @@ public class Window {
 
         init();
         loop();
+
+        glfwFreeCallbacks(glfwWindow);
+        glfwDestroyWindow(glfwWindow);
+
+        glfwTerminate();
+        glfwSetErrorCallback(null).free();
     }
 
     private void init() {
@@ -61,6 +74,13 @@ public class Window {
             throw new IllegalStateException("Failed to create the GLFW window.");
         }
 
+//        Mouse callbacks
+        glfwSetCursorPosCallback(glfwWindow, MouseListener::mousePosCallback);
+        glfwSetMouseButtonCallback(glfwWindow, MouseListener::mouseButtonCallback);
+        glfwSetScrollCallback(glfwWindow, MouseListener::mouseScrollCallback);
+
+//        Key callbacks
+        glfwSetKeyCallback(glfwWindow, KeyListener::keyCallback);
         // Make OpenGL context current
         glfwMakeContextCurrent(glfwWindow);
         // Enable v-sync
@@ -81,13 +101,29 @@ public class Window {
 
         // Run the rendering loop until the user has attempted to close
         // the window or has pressed the ESCAPE key.
-        while ( !glfwWindowShouldClose(glfwWindow) && glfwGetKey(glfwWindow,GLFW_KEY_ESCAPE) != GLFW_TRUE) {
+        while ( !glfwWindowShouldClose(glfwWindow) && !KeyListener.isKeyPressed(GLFW_KEY_ESCAPE)) {
             // Poll for window events. The key callback above will only be
             // invoked during this call.
             glfwPollEvents();
 
-            glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+            glClearColor(r, g, b, a);
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+
+            if (fadeToBlack){
+                r = Math.max(r-0.01f, 0);
+                g = Math.max(g-0.01f, 0);
+                b = Math.max(b-0.01f, 0);
+            }
+
+            if (r == 0 && g == 0 && b == 0){
+                fadeToBlack = false;
+                r = 1;
+                g = 1;
+                b = 1;
+            }
+            if(KeyListener.isKeyPressed(GLFW_KEY_SPACE)){
+                fadeToBlack = true;
+            }
 
             glfwSwapBuffers(glfwWindow); // swap the color buffers
         }
